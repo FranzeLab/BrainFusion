fixed memory overflow

BrainFusion/_transform_2Dmap.py
@@ -8,12 +8,10 @@ def transform_grid2contour(original_contour, deformed_contour, original_grid):
                                             deformed_contour=deformed_contour)
 
     # Transform original contour to the coordinate system of the deformed contour
-    trafo_cont_x, trafo_cont_y = evaluate_transformation(rbf_x, rbf_y, original_contour)
-    trafo_contour = np.vstack([trafo_cont_x, trafo_cont_y]).T
+    trafo_contour = np.array([[rbf_x(x, y), rbf_y(x, y)] for x, y in original_contour])
 
     # Transform original grid to the coordinate system defined by the average contour
-    trafo_grid_x, trafo_grid_y = evaluate_transformation(rbf_x, rbf_y, original_grid)
-    trafo_grid = np.vstack([trafo_grid_x, trafo_grid_y]).T
+    trafo_grid = np.array([[rbf_x(x, y), rbf_y(x, y)] for x, y in original_grid])
 
     return trafo_grid, trafo_contour
 
@@ -27,14 +25,6 @@ def create_rbf_interpolators(original_contour, deformed_contour):
     rbf_y = Rbf(x_orig, y_orig, y_deform, function='linear', smooth=0.2)
 
     return rbf_x, rbf_y
-
-
-def evaluate_transformation(rbf_x, rbf_y, grid_points):
-    grid_x, grid_y = grid_points[:, 0], grid_points[:, 1]
-    trafo_grid_x = rbf_x(grid_x, grid_y)
-    trafo_grid_y = rbf_y(grid_x, grid_y)
-
-    return trafo_grid_x, trafo_grid_y
 
 
 def extend_grid(regular_grid, x_extend, y_extend):